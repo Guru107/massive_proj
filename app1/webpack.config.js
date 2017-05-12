d7b546dde3adce4fe41e29bd61567262d80252fb
Jigar Mehta <jigar.mehta@bookmyshow.com>
2017-05-12 20:44:54 +0530

    Add package son and web pack style

--- app1/webpack.config.js
+++ app1/webpack.config.js
@@ -18,7 +18,8 @@ module.exports = {
   module: {
 	loaders: [
 	  { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-	  { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+	  { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/},
+		{ test: /\.css$/, loader: 'style-loader!css-loader', exclude: /node_modules/ }
 	]
   },
 	plugins: [HtmlWebpackPluginConfig]
