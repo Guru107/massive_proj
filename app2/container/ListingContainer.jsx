d33d2a18b00ca455ce67b8e470753a7dc945571d
Jigar Mehta <jigar.mehta@bookmyshow.com>
2017-05-12 19:38:13 +0530

    Add app1 and app2

---
+++ app2/container/ListingContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class ListingContainer extends React.Component {
+  render() {
+	return (
+	 <div style={{textAlign: 'center'}}>
+		<h1>Hello World FROM APP2</h1>
+	  </div>)
+  }
+}
