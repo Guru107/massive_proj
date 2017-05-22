86f6993dc343c9c5476deecf9b277e0ba3313a5f
Jigar Mehta <jigar.mehta@bookmyshow.com>
2017-05-22 13:47:48 +0530

    Add new web module

---
+++ web_modules/new/index.js
@@ -0,0 +1,7 @@
+console.log(process.env)
+if(process.env.NODE_ENV === 'development'){
+	//require('babel-register')
+	module.exports = require('./src/index.js')
+}else{
+	module.exports = require('./lib/index.js')
+}
---
+++ web_modules/new/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+class New extends React.Component {
+
+	render(){
+		return (
+			<div className="test">
+				<ul>
+					<li>This idasds from new List Item 1221</li>
+				</ul>
+			</div>
+		)
+	}
+}
+
+export default New
